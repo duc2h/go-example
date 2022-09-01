d311fd96ab17db660cc2d06803f4cb2f95974d83
edarha <hoangduc02.01.1998@gmail.com>
2022-09-01 23:41:57 +0700

    check cpu

---
+++ channel/example/go.mod
@@ -0,0 +1,5 @@
+module edarha-channel
+
+go 1.18
+
+require github.com/mackerelio/go-osstat v0.2.3
--- channel/example/main.go
+++ channel/example/main.go
@@ -1,42 +1,76 @@
-// write a mock program download 10k record. in order to increasing speed, we will use worker pool.
+// write a mock program download 10k record. in order to increasing speed, we will use worker pool to run .
 
 package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 	"sync"
 	"time"
+
+	"github.com/mackerelio/go-osstat/cpu"
 )
 
 const (
-	records = 100
+	records = 10000
 	workers = 5
 )
 
 func main() {
+	before, err := cpu.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
 	mapA := make(map[int]int)
 	mu := sync.Mutex{}
 	queue := make(chan int, 100)
+	runtime.GOMAXPROCS(5)
+	procs := runtime.GOMAXPROCS(0)
+	numcpu := runtime.NumCPU()
+
+	defer timeTracker(time.Now())
+
+	fmt.Println("cpu: ", numcpu)
+	fmt.Println("procs1: ", procs)
 
 	wg := &sync.WaitGroup{}
 
 	wg.Add(workers + 1)
 
 	go download(queue, wg)
-	for i := 0; i < 5; i++ {
+	for i := 0; i < workers; i++ {
 		go process(mapA, &mu, queue, i, wg)
 
 	}
 
 	wg.Wait()
+	fmt.Println("procs3: ", procs)
 
 	fmt.Println(mapA)
-	total := 0
+	total1 := 0
 	for _, v := range mapA {
-		total += v
+		total1 += v
 	}
 
-	fmt.Println(total)
+	fmt.Println(total1)
+
+	after, err := cpu.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
+	total := float64(after.Total - before.Total)
+	fmt.Printf("cpu user: %f %%\n", float64(after.User-before.User)/total*100)
+	fmt.Printf("cpu system: %f %%\n", float64(after.System-before.System)/total*100)
+	fmt.Printf("cpu idle: %f %%\n", float64(after.Idle-before.Idle)/total*100)
+}
+
+func timeTracker(start time.Time) {
+	track := time.Since(start)
+
+	fmt.Println("this program took ", track)
 }
 
 func appendMap(b *sync.Mutex, key int, mapA map[int]int) {
@@ -52,7 +86,7 @@ func download(queue chan<- int, wg *sync.WaitGroup) {
 	for i := 1; i <= records; i++ {
 		queue <- i
 
-		fmt.Printf("%d has been enqueued \n", i)
+		// fmt.Printf("%d has been enqueued \n", i)
 	}
 	close(queue)
 
@@ -61,10 +95,10 @@ func download(queue chan<- int, wg *sync.WaitGroup) {
 // write a function receive data.
 func process(m map[int]int, mu *sync.Mutex, queue <-chan int, index int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := range queue {
+	for range queue {
 		appendMap(mu, index, m)
-		time.Sleep(time.Second)
+		time.Sleep(time.Microsecond * 500)
 
-		fmt.Printf("index: %d, value: %d \n", index, i)
+		// fmt.Printf("index: %d, value: %d \n", index, i)
 	}
 }
