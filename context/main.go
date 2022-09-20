16465637e86e4464652891704ae122a343e0ca52
edarha <hoangduc02.01.1998@gmail.com>
2022-09-20 17:35:21 +0700

    add context

---
+++ context/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	go f1(ctx)
+	go f2(ctx)
+
+	time.Sleep(time.Second * 3)
+}
+
+func f1(ctx context.Context) {
+	<-ctx.Done()
+
+	fmt.Println("====== f1 ======")
+}
+
+func f2(ctx context.Context) {
+	<-ctx.Done()
+
+	fmt.Println("====== f2 ======")
+}
